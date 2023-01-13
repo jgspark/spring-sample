e340027cd1822d737a9324ba805e45596865a96b
spark <pnw123456@icloud.com>
2023-01-13 23:09:24 +0900

    build: spring version downgrade
    - java version down
    - h2 config add

--- src/main/java/com/example/contract/ContractApplication.java
+++ src/main/java/com/example/contract/ContractApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ContractApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(ContractApplication.class, args);
     }
