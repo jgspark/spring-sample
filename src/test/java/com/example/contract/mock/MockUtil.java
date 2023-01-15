f9b8bb2a0d93ece235bf83dedd8d0e7c323f67c9
spark <pnw123456@icloud.com>
2023-01-15 11:29:38 +0900

    test: 상품 담보 조회 테스트 케이스

--- src/test/java/com/example/contract/mock/MockUtil.java
+++ src/test/java/com/example/contract/mock/MockUtil.java
@@ -1,5 +1,7 @@
 package com.example.contract.mock;
 
+import com.example.contract.doamin.Product;
+import com.example.contract.doamin.Warrant;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -7,7 +9,10 @@ import org.springframework.core.io.ClassPathResource;
 
 import java.io.File;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.nio.file.Files;
+import java.util.Collections;
+import java.util.Map;
 
 public class MockUtil {
 
@@ -38,5 +43,21 @@ public class MockUtil {
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static Warrant convert(Map map) {
+        return Warrant.createBuilder()
+                .title((String) map.get("title"))
+                .subscriptionAmount(new BigDecimal((Integer) map.get("subscriptionAmount")))
+                .standardAmount(new BigDecimal((Integer) map.get("standardAmount")))
+                .build();
+    }
+
+    public static Product convert(Product mock , Warrant warrant){
+        return Product.createBuilder()
+                .title(mock.getTitle())
+                .term(mock.getTerm())
+                .warrants(Collections.singleton(warrant))
+                .build();
     }
 }
--- src/test/java/com/example/contract/repository/ProductRepositoryTest.java
+++ src/test/java/com/example/contract/repository/ProductRepositoryTest.java
@@ -1,13 +1,16 @@
 package com.example.contract.repository;
 
 import com.example.contract.doamin.Product;
+import com.example.contract.doamin.Warrant;
 import com.example.contract.mock.MockUtil;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Collections;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,28 +21,93 @@ class ProductRepositoryTest {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private WarrantRepository warrantRepository;
+
+    private Warrant mockWarrant;
+
+    @BeforeEach
+    public void init() {
+        Map<String, Object> map = MockUtil.readJson("json/product/repository/init.json", Map.class);
+
+        Warrant mock = MockUtil.convert((Map) map.get("warrant"));
+
+        this.mockWarrant = warrantRepository.saveAndFlush(mock);
+    }
 
     @Test
+    @Order(1)
     @DisplayName("상품 생성")
     public void save_ok() {
 
-        Product mock = MockUtil.readJson("json/product/save_ok.json", Product.class);
+        Warrant findWarrant = warrantRepository.findById(mockWarrant.getId()).orElseThrow(RuntimeException::new);
+
+        Product mock = MockUtil.convert(MockUtil.readJson("json/product/repository/save_ok.json", Product.class), findWarrant);
 
         Product entity = productRepository.save(mock);
+
+        productRepository.flush();
 
         assertNotNull(entity.getId());
         assertEquals(mock.getTitle(), entity.getTitle());
         assertEquals(mock.getTerm(), entity.getTerm());
+        assertEquals(entity.getWarrants(), mock.getWarrants());
     }
 
     @Test
+    @Order(2)
     @DisplayName("계약 기간이 조건에 맞지 않는 케이스")
     public void save_fail_case1() {
 
-        Product mock = MockUtil.readJson("json/product/save_fail_case1.json", Product.class);
+        Product mock = MockUtil.readJson("json/product/repository/save_fail_case1.json", Product.class);
 
         assertThrows(RuntimeException.class, () -> {
             productRepository.save(mock);
         });
+    }
+
+    @Nested
+    @DisplayName("조회 테스트 케이스")
+    public class Select {
+
+        private Product mock;
+
+        @BeforeEach
+        public void init() {
+
+            Warrant findWarrant = warrantRepository.findById(mockWarrant.getId()).orElseThrow(RuntimeException::new);
+
+            mock = productRepository.save(MockUtil.convert(MockUtil.readJson("json/product/repository/findByIdAndWarrants_IdIn_ok.json", Product.class), findWarrant));
+
+            productRepository.flush();
+        }
+
+        @Test
+        @Order(1)
+        @DisplayName("상품 아이디 와 담보 아이디 별 in 절 조회 성공 케이스")
+        public void findByIdAndWarrants_IdIn_ok() {
+
+            Product entity = productRepository.findByIdAndWarrants_IdIn(mock.getId(), Collections.singleton(mockWarrant.getId())).orElseThrow(RuntimeException::new);
+
+            assertEquals(entity.getId(), mock.getId());
+            assertEquals(entity.getTitle(), mock.getTitle());
+            assertEquals(entity.getTerm(), mock.getTerm());
+            assertEquals(entity.getWarrants(), mock.getWarrants());
+        }
+
+        @Test
+        @Order(2)
+        @DisplayName("상품 아이디 와 담보 아이디 별 in 절 조회 실패 케이스")
+        public void findByIdAndWarrants_IdIn_fail1() {
+
+            Long mockId = 10000000L;
+
+            assertThrows(RuntimeException.class, () -> {
+                productRepository.findByIdAndWarrants_IdIn(mockId, Collections.singleton(mockWarrant.getId())).orElseThrow(RuntimeException::new);
+            });
+
+        }
+
+
     }
 }
