55906429f89d6cba8187979fc8d949d7e16326a5
spark <pnw123456@icloud.com>
2023-03-04 10:08:55 +0900

    refactor: 체크 로직 테스트 케이스 리팩터링
    - 각각의 케이스 별로 테스트 케이스를 작성하는 방식이 아닌, 파라미터를 넘겨주는 방식으로 변경

--- src/test/java/com/example/contract/doamin/warrant/WarrantTest.java
+++ src/test/java/com/example/contract/doamin/warrant/WarrantTest.java
@@ -3,9 +3,14 @@ package com.example.contract.doamin.warrant;
 import com.example.contract.domain.warrant.Warrant;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 
 import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -16,39 +21,40 @@ class WarrantTest {
     @Nested
     @DisplayName("보험로 계산 로직은")
     class GetPremiumMethod {
-
-        @Test
+        @ParameterizedTest(name = "보험료는 {0}, 담보데이터는 {1} 이다.")
+        @ArgumentsSource(PremiumOkArgs.class)
         @DisplayName("성공을 하게 된다.")
-        public void getPremium_ok() {
-
-            Warrant warrant = Warrant.createBuilder().title("상해치료").subscriptionAmount(new BigDecimal(1000000)).standardAmount(new BigDecimal(100)).build();
-
+        public void getPremium_ok(BigDecimal m , Warrant warrant) {
             BigDecimal n = warrant.getPremium();
-
-            BigDecimal m = new BigDecimal(10000);
-
             assertEquals(0, m.compareTo(n));
         }
 
-        @Test
-        @DisplayName("SubscriptionAmount 가 Null 이면 Exception 이 발생하게 된다.")
-        public void getPremium_fail1() {
-
-            Warrant warrant = Warrant.createBuilder().title("상해치료").standardAmount(new BigDecimal(100)).build();
-
+        @ArgumentsSource(PremiumFailArgs.class)
+        @DisplayName("예외를 발생을 하게 된다.")
+        @ParameterizedTest(name = "담보 데이터를 {0} 으로 전달하면 예외가 발생이 된다.")
+        public void getPremium_fail2(Warrant warrant) {
             assertThrows(IllegalArgumentException.class, warrant::getPremium);
-
-        }
-
-        @Test
-        @DisplayName("StandardAmount 가 Null 이면 Exception이 발생하게 된다.")
-        public void getPremium_fail2() {
-
-            Warrant warrant = Warrant.createBuilder().title("상해치료").subscriptionAmount(new BigDecimal(1000000)).build();
-
-            assertThrows(IllegalArgumentException.class, warrant::getPremium);
-
         }
     }
 
+    static class PremiumOkArgs implements ArgumentsProvider {
+
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
+            return Stream.of(
+                    Arguments.of(new BigDecimal(10000), Warrant.createBuilder().title("상해치료").subscriptionAmount(new BigDecimal(1000000)).standardAmount(new BigDecimal(100)).build())
+            );
+        }
+    }
+
+    static class PremiumFailArgs implements ArgumentsProvider {
+
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
+            return Stream.of(
+                    Arguments.of(Warrant.createBuilder().title("상해치료").subscriptionAmount(new BigDecimal(1000000)).build()),
+                    Arguments.of(Warrant.createBuilder().title("상해치료").standardAmount(new BigDecimal(100)).build())
+            );
+        }
+    }
 }
